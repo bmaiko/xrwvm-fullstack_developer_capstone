3532e000ffee1160579803ee564af75863da7dc2
bmaiko <maiko-blaukopf@hotmail.com>
2025-08-19 05:29:39 +0200

    Update settings.py
    update

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,15 +28,26 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost',
-    ('https://maikoblaukop-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'),
-    ('https://maikoblaukop-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'),
-    ('https://maikoblaukop-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/admin')
+ALLOWED_HOSTS = [
+    'localhost',
+    (
+        'https://ripochesierr-8000.theianext-0-labs-prod-misc-tools-us-east-0.'
+        'proxy.cognitiveclass.ai'
+    ),
+    (
+        'https://ripochesierr-8000.theiadockernext-1-labs-prod-theiak8s-4-'
+        'tor01.proxy.cognitiveclass.ai'
+    )
 ]
-CSRF_TRUSTED_ORIGINS=[
-    ('https://maikoblaukop-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'),
-    ('https://maikoblaukop-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'),
-    ('https://maikoblaukop-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/admin')
+CSRF_TRUSTED_ORIGINS = [
+    (
+        'https://ripochesierr-8000.theianext-0-labs-prod-misc-tools-us-east-0.'
+        'proxy.cognitiveclass.ai'
+    ),
+    (
+        'https://ripochesierr-8000.theiadockernext-0-labs-prod-theiak8s-4-'
+        'tor01.proxy.cognitiveclass.ai'
+    ),
 ]
 
 REST_FRAMEWORK = {
@@ -102,7 +113,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
